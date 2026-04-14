57907f310c0890aac7ca29a84c55b50e9a0bb285
trk <totoglu@gmail.com>
2026-04-14 15:06:35 +0300

    chore(htmx): bump module version to 116

--- Htmx.module.php
+++ Htmx.module.php
@@ -46,7 +46,7 @@ class Htmx extends WireData implements Module, ConfigurableModule
     {
         return [
             'title' => 'HTMX',
-            'version' => 115,
+            'version' => 116,
             'summary' => 'Provides HTMX v2 integration including Component State, Out-of-band swaps, Extensions, and SSE support natively within ProcessWire.',
             'href' => 'https://github.com/trk/Htmx',
             'author' => 'Iskender TOTOGLU @trk @ukyo',
